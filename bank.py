386d12331ee3311047d76dc42878d2aa43fdb7b3
ash408 <124532093+ash408@users.noreply.github.com>
2023-08-18 09:10:07 -0500

    Created banks, setup TODO template

---
+++ bank.py
@@ -0,0 +1,44 @@
+
+#Defines functions for counting denominations
+class BankInterface():
+	'''
+	Interfaced used for creation of banks
+
+	...
+
+	Attributes
+	==========
+	variable : type
+		description
+
+	Methods
+	=======
+	TODO(parameters)
+		Description
+		returns ...
+	'''
+	def __init__(self):
+		pass
+
+	def TODO(self):
+		'''
+		Description		
+	
+			Parameters:
+				name (type): description
+
+			Returns:
+				name (type): description
+		'''
+		pass
+
+
+class Drawer(BankInterface):
+
+	def __init__(self):
+		pass
+
+class Safe(BankInterface):
+
+	def __init__(self):
+		pass
