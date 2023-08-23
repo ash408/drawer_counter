77e4e8f23ab2e2391c87bb4084f36ca5e73fc822
ash408 <124532093+ash408@users.noreply.github.com>
2023-08-23 16:27:20 -0500

    Added fifty denomination

--- money.py
+++ money.py
@@ -49,6 +49,7 @@ class PoolInterface():
 				remainder (double): left over amount after denominations
 								are removed
 		'''
+		pass
 
 
 class ChangePool(PoolInterface):
@@ -68,5 +69,6 @@ class BillPool(PoolInterface):
 			  		  			('five'    :   5),
 			  		  			('ten'     :  10),
 			  		  			('twenty'  :  20),
+								('fifty'	 :  50),
 								('hundred' : 100) ])
 
