379406ce20047b22d6348e823a5ff5c06c38b484
ash408 <124532093+ash408@users.noreply.github.com>
2023-09-02 07:45:01 -0500

    Added PoolInterface initialization test

--- tests/money_tests.py
+++ tests/money_tests.py
@@ -1,11 +1,20 @@
 
 import unittest
 
+from ..money import PoolInterface
 
 class TestPoolInterface(unittest.TestCase):
 
-	def test (self):
-		pass
+	def test_interface_initialization (self):
+		VALUES = OrderedDict([('a' : 0.1), ('b', 0.5)])
+
+		interface = PoolInterface(VALUES)
+
+		self.assertEqual(interface.VALUES['a'], 0.1)
+		self.assertEqual(interface.VALUES['b'], 0.5)
+
+		self.assertEqual(interface.amounts['a'], 0)
+		self.assertEqual(interface.amounts['b'], 0)
 
 
 if __name__ == '__main__':
